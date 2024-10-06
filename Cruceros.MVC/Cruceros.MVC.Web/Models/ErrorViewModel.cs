424d187c8e0f2ba35c03c83eba6751881b04b819
Julián Guarnieri <102745245+jguarnieri97@users.noreply.github.com>
2024-10-06 11:56:22 -0300

    Initial MVC

---
+++ Cruceros.MVC/Cruceros.MVC.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Cruceros.MVC.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
