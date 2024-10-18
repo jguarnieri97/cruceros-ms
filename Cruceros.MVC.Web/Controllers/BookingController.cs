9c9d5cac66a7e7ab1e0ca344680d551ab51c28e2
patricio-pk <kol.patricio.pk@gmail.com>
2024-10-18 20:05:35 -0300

    Layouts, Vistas y Controllers
    Se agregaron todas las vistas y los controllers necesarios para arrancar

---
+++ Cruceros.MVC.Web/Controllers/BookingController.cs
@@ -0,0 +1,27 @@
+﻿using Cruceros.Data.Entidades;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cruceros.MVC.Web.Controllers
+{
+    public class BookingController : Controller
+    {
+        private CrucerosContext _ctx;
+
+        public BookingController(CrucerosContext ctx)
+        {
+            _ctx = ctx;
+        }
+
+        public ActionResult BookingSuccess()
+        {
+            return View();
+        }
+
+        public ActionResult ConfirmBooking()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ Cruceros.MVC.Web/Controllers/UserController.cs
@@ -0,0 +1,31 @@
+﻿using Cruceros.Data.Entidades;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cruceros.MVC.Web.Controllers
+{
+    public class UserController : Controller
+    {
+        private CrucerosContext _ctx;
+
+        public UserController(CrucerosContext ctx)
+        {
+            _ctx = ctx;
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        public ActionResult SignUp()
+        {
+            return View();
+        }
+
+        public ActionResult UserProfile()
+        {
+            return View();
+        }
+    }
+}
